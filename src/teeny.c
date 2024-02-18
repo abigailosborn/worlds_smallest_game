3d7173dfd1f372d2a5593aab7b4c349483e3a573
abigailosborn <the0ther31415@gmail.com>
2024-02-18 12:45:02 -0500

    Cube movemnet

--- src/teeny.c
+++ src/teeny.c
@@ -4,6 +4,7 @@ int main() {
     const int screenWidth = 800;
     const int screenHeight = 450;
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    //Vector2 ballPosition = {(float)screenWidth/2, (float)screenHeight/2};
     Camera3D camera = { 0 };
     camera.position = (Vector3){10.0f, 10.0f, 10.0f};
     camera.target = (Vector3){0.0f, 0.0f, 0.0f};
@@ -16,21 +17,26 @@ int main() {
 
     while (!WindowShouldClose())
     {
+        if (IsKeyDown(KEY_RIGHT)) cubePosition.x += 1.0f;
+        if (IsKeyDown(KEY_LEFT)) cubePosition.x -= 1.0f;
+        if (IsKeyDown(KEY_UP)) cubePosition.z -= 1.0f;
+        if (IsKeyDown(KEY_DOWN)) cubePosition.z += 1.0f;
         BeginDrawing();
 
             ClearBackground(GetColor(0x2e1038FF));
-            DrawText("Congrats! Cube!", (screenWidth / 4), 40, 20, GetColor(0xe5c7efFF));
+            DrawText("Move Cube Using Arrow Keys", (screenWidth / 4), 40, 20, GetColor(0xe5c7efFF));
             
 
             BeginMode3D(camera);
    
                 //DrawRectangle(100, 100, 100, 100, RED);
                 //DrawTriangle((Vector2){200, 100}, (Vector2){250, 75}, (Vector2){100, 100}, BLUE);
-                //DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, GREEN);
+                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, GREEN);
                 DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, DARKGREEN);
-                DrawSphere((Vector3){0.0f, 0.0f, 0.0f}, 2.0f, BLUE);
-                DrawSphereWires((Vector3){0.0f, 0.0f, 0.0f}, 2.0f, 20, 20, DARKBLUE);
-                DrawGrid(10, 1.0f);
+                //DrawSphere((Vector3){0.0f, 0.0f, 0.0f}, 2.0f, BLUE);
+                //DrawSphereWires((Vector3){0.0f, 0.0f, 0.0f}, 2.0f, 20, 20, DARKBLUE);
+                //DrawCircleV(ballPosition, 50, BLUE);
+                DrawGrid(20, 1.0f);
 
             EndMode3D();
 
